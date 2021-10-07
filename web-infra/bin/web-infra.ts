2ef95bb70c2ca76e718d2aaeda99cb998e61a702
suman kalapatapu <suman.kalapatapu@gmail.com>
2021-10-07 19:08:44 -0400

    environmental variables setup

--- web-infra/bin/web-infra.ts
+++ web-infra/bin/web-infra.ts
@@ -5,22 +5,22 @@ import { WebInfraStack } from '../lib/web-infra-stack';
 
 const app = new cdk.App();
 
-function requiredContextVar(propName: string): string{
+// function requiredContextVar(propName: string): string{
     
-    let val  = app.node.tryGetContext(propName);
-    console.log(val)
-    if (!val) {
-        throw new Error("Required environment variables");
-    }
-    return val;
-}
+//     let val  = app.node.tryGetContext(propName);
+//     console.log(val)
+//     if (!val) {
+//         throw new Error("Required environment variables");
+//     }
+//     return val;
+// }
 
-let envProps =  app.node.tryGetContext(requiredContextVar("env"));
-console.log(envProps)
+// let envProps =  app.node.tryGetContext(requiredContextVar("env"));
+// console.log(envProps)
 
 new WebInfraStack(app, 'WebInfraStack', {
     env: {
-        account: "156900288200",
-        region: "us-east-1"
+        account: process.env.AWS_ACCOUNT_PILOT,
+        region:  process.env.AWS_REGION_PILOT
     }
 });
